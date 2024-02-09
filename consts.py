3199900be9da181ad99499abbb0b1b97432e3d66
MdrskJ <modorskiy.jakov@gmail.com>
2024-02-09 19:10:15 +0500

    Append Window

---
+++ consts.py
@@ -0,0 +1,4 @@
+ui_path = "map.ui"
+
+W, H = SIZE = 300, 400
+X, Y = 400, 400
---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import uic
+from PyQt5.QtGui import QPixmap
+
+from consts import *
+
+
+class MyWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi(ui_path, self)
+        self.loadInfo()
+
+    def loadInfo(self):
+        self.setFixedSize(*SIZE)
+        self.setWindowTitle('')
+        self.setStyleSheet(
+            "background-color: rgb(50, 50, 50);"
+            "color: rgb(255, 255, 255);"
+            "border: 0px solid #094065;"
+        )
+
+        self.pict.resize(W, H)
+        pixmap = QPixmap(f'pict1.jpg')
+        pixmap = pixmap.scaled(W, H)
+        self.pict.setPixmap(pixmap)
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.showMaximized()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
