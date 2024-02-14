619599ecdc350c3d53e52ee649baa877e58fcd59
MdrskJ <modorskiy.jakov@gmail.com>
2024-02-14 12:02:53 +0500

    Commit with changes to task number 2, added pgUp and pgDown combinations

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.Qt import QImage
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget
 
 from consts import *
 from util import get_pict
+
+spn = 0.004
 
 
 class MyWidget(QWidget):
@@ -25,10 +28,23 @@ class MyWidget(QWidget):
         )
 
         self.pict.resize(W, H)
+        self.update_pict(spn)
 
-        path = get_pict(lon, lat, spn_lon, spn_lat)
+    def keyPressEvent(self, event):
+        global spn
+        if spn + d < 180 and event.key() == Qt.Key_Up:
+            spn = round(spn + d, 3)
+            self.update_pict(spn)
+            print(spn)
+
+        if spn - d > 0.001 and event.key() == Qt.Key_Down:
+            spn = round(spn - d, 3)
+            self.update_pict(spn)
+            print(spn)
+
+    def update_pict(self, new_spn: float):
+        path = get_pict(lon, lat, str(new_spn))
         pixmap = QPixmap(QImage(path))
-
         self.pict.setPixmap(pixmap)
 
 
--- util.py
+++ util.py
@@ -1,20 +1,23 @@
 from io import BytesIO
+import os.path
 
 import requests
 from PIL import Image
 
 
-def get_pict(lon: str, lat: str, spn_lon: str, spn_lat: str):
+def get_pict(lon: str, lat: str, spn: str):
+    map_path = f"data/map_image_spn={spn}.jpg"
+    if os.path.isfile(map_path):
+        return map_path
+
     params = {
         "ll": ",".join([lon, lat]),
-        "spn": ",".join([spn_lon, spn_lat]),
+        "spn": ",".join([spn, spn]),
         "l": "map"
     }
     map_api_server = "http://static-maps.yandex.ru/1.x/"
     response = requests.get(map_api_server, params=params)
     map_image = Image.open(BytesIO(response.content))
     map_image = map_image.convert("RGB")
-    map_path = "data/map_image.jpg"
     map_image.save(map_path, 'JPEG')
-
     return map_path
