4397bedd4076bcc1ee31fb3a740ff936ad07d056
MdrskJ <modorskiy.jakov@gmail.com>
2024-02-16 11:21:32 +0500

    Commit with changes to task number 2, version: 5, file name depends on map params

--- util.py
+++ util.py
@@ -6,15 +6,15 @@ from PIL import Image
 
 
 def get_pict(lon: str, lat: str, z: str):
-    map_path = f"data/map_image_ll={lon},{lat},z={z}"
-    if os.path.isfile(map_path):
-        return map_path
-
     params = {
         "ll": ",".join([lon, lat]),
         "z": z,
         "l": "map",
     }
+    map_path = "data/map_image_" + "_".join([f"{i}={params[i]}" for i in params])
+    if os.path.isfile(map_path):
+        return map_path
+
     map_api_server = "http://static-maps.yandex.ru/1.x/"
     response = requests.get(map_api_server, params=params)
     map_image = Image.open(BytesIO(response.content))
